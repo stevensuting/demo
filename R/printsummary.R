1bf07fe6a439bbe42b3bc465626e1bae51ac766c
Steven <steven.suting@gmail.com>
2015-07-13 17:57:45 +0530

    update

--- R/printsummary.R
+++ R/printsummary.R
@@ -1,1 +1,9 @@
-printsummary <-function(data){View(data);}
+printsummary <- function(mydata){
+  #override default 
+  options(max.print=99999999);
+  options(width=120);
+  
+  #print
+  print(summary(mydata))
+  invisible()
+}
