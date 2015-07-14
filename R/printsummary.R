f402ef2580de7f200021d251e7a0cec4a3eba15c
Steven <steven.suting@gmail.com>
2015-07-14 16:07:09 +0530

    u

--- R/printsummary.R
+++ R/printsummary.R
@@ -2,10 +2,16 @@ printsummary <- function(mydata){
   #override default 
   options(max.print=99999999);
   options(width=180);
+  a <- "**********************************DATA SET**********************************"
+  b <- "**********************************Aggregate**********************************"
   
   #print
+  print(a) 
+  print(mydata)
+
+  print(b)  
   mydata <- aggregate(mydata[,2], by=list(mydata[,1]), FUN=sum)
   print(mydata)
-invisible();
+  invisible();
   
 }
