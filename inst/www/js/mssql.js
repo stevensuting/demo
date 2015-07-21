1dbbe3ac648160d9067e979cc22692bd417a9539
Steven <steven.suting@gmail.com>
2015-07-21 11:47:23 +0530

    ss

--- inst/www/js/mssql.js
+++ inst/www/js/mssql.js
@@ -21,8 +21,8 @@ $("submitbutton").click(function(){
        // var myname = $("#namefield").val();
         
         //perform the request
-        var req = ocpu.call("sum", {
-          myname : myname
+        var req = ocpu.call("sum()", {
+         // myname : myname
         }, function(output){
           $("#output").text(output.message);
         });
