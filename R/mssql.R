f23b631279ba8a0c7af67398e8309dbd388beaab
Steven <steven.suting@gmail.com>
2015-07-21 13:15:08 +0530

    ss

--- R/mssql.R
+++ R/mssql.R
@@ -1,5 +1,6 @@
 mssql <- function(){
 driver <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver", "../inst/www/etc/sqljdbc4.jar")
 conn.JDBC <- dbConnect(driver,"jdbc:sqlserver://192.168.2.113;databaseName=MicrosoftBIRetailDemo","appuser","appuser123")
-dbGetQuery(conn.JDBC, "select top 10 * from FactOnlineSales;")
+data<- dbGetQuery(conn.JDBC, "select top 10 * from FactOnlineSales;")
+data
 }
