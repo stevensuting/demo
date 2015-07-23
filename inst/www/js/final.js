4330ec803d31af56d809667bf658052da27ac719
santosh.k <santosh.k@winnow.in>
2015-07-23 14:00:24 +0530

    u-santosh
    updated json

--- inst/www/js/final.js
+++ inst/www/js/final.js
@@ -1,31 +1,45 @@
-function finale()
-{
+var app = angular.module('Demo', ['ui.bootstrap', 'ui.directives']);
 
-$.getJSON('data.json', function(data) {
-        var output;
-        for (var i in data.users) {
-            output=data.users[i].firstName + " " + data.users[i].lastName + "--" + data.users[i].joined.month;
-//        alert(output);
-        document.write("<div>");
+app.controller("AppController", function ($scope, $http) {
+    $scope.loadUsersrefresh = function ()
+    {
+alert("hiiiiiiiiiiiiiiiii");
+//        $.getJSON('./json/data.json', function (data) {
+var text = '{"employees":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+            var output;
+            obj = JSON.parse(text);
+$scope.UsersData = obj.employees;
+alert($scope.UsersData);
+            for (var i in obj.employees) {
+                output = obj.employees[i].firstName + " " + obj.employees[i].lastName + "--" +obj.employees[i].joined.month;
+                alert(output);
+//                $scope.UsersData = output;
+//                alert($scope.UsersData);
+//                document.write("<div>");
 //    for(var j = 0; j < key[i].length; j++)
 //    {
-       document.write("<input type='button' value='" + data.users[i].firstName + "' onclick = call('"+data.users[i].firstName+"') />");
-       
+//                document.write("<input type='button' class='btn btn-primary dropdown-toggle' value='" + data.users[i].firstName + "' onclick = call('" + data.users[i].firstName + "') />");
+
 //    }
-    document.write("</div>");
-        }
+//                document.write("</div>");
+            }
 
 //        output+="</ul>";
-        
+
 //         document.getElementById("placeholder").innerHTML=output;
-  });
-    
-}
+//        });
+
+    }
 
 
-function call(aaa)
-{
-    alert("asdasd  "+aaa);
-    
-}
+    function call(aaa)
+    {
+        alert("asdasd  " + aaa);
+
+    }
+});
+
 
