36e57058b1f4d0dd91ee01058071d375930223ad
santosh.k <santosh.k@winnow.in>
2015-07-29 11:25:03 +0530

    u-santosh

--- inst/www/js/chart.js
+++ inst/www/js/chart.js
@@ -97,29 +97,33 @@ $(function () {
     });
 
     /*X-axis values change*/
+    var a=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
     $('#sub').click(function () {
-            Highcharts.charts[0].xAxis[0].update({categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']}, true);
+            Highcharts.charts[0].xAxis[0].update({categories: a}, true);
     });
 
+var b=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $('#sub1').click(function () {
-        Highcharts.charts[0].xAxis[0].update({categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}, true);
+        Highcharts.charts[0].xAxis[0].update({categories: b}, true);
     });
 
+var c=['1-Jan-2014', '1-Feb-2014', '1-Mar-2014', '1-Apr-2014', '1-May-2014', '1-Jun-2014', '1-Jul-2014', '1-Aug-2014', '1-Sep-2014', '1-Oct-2014', '1-Nov-2014', '1-Dec-2014'];
     $('#sub2').click(function () {
-        Highcharts.charts[0].xAxis[0].update({categories: ['1-Jan-2014', '1-Feb-2014', '1-Mar-2014', '1-Apr-2014', '1-May-2014', '1-Jun-2014', '1-Jul-2014', '1-Aug-2014', '1-Sep-2014', '1-Oct-2014', '1-Nov-2014', '1-Dec-2014']}, true);
+        Highcharts.charts[0].xAxis[0].update({categories: c}, true);
     });
 
+var x=[14.5, 18.2, 1.5, 5.2, 26.5, 23.3, 18.3, 13.9];
     /*Y-axis Value Changes*/
     $('#add').click(function () {
-        Highcharts.charts[0].series[0].setData([14.5, 18.2, 1.5, 5.2, 26.5, 23.3, 18.3, 13.9],true);
+        Highcharts.charts[0].series[0].setData(x,true);
     });
-
+var y=[7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6];
     $('#add1').click(function () {
-        Highcharts.charts[0].series[0].setData([7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],true);
+        Highcharts.charts[0].series[0].setData(y,true);
     });
-
+var z=[7.0, 9.5, 18.2, 21.5,  26.5, 23.3, 13.9, 9.6];
     $('#add2').click(function () {
-        Highcharts.charts[0].series[0].setData([7.0, 9.5, 18.2, 21.5,  26.5, 23.3, 13.9, 9.6],true);
+        Highcharts.charts[0].series[0].setData(z,true);
         
     });
 
