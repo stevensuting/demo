9b1fb2c6300270c952492a7688d1cb8124a306d4
hkvin <hkvvin@gmail.com>
2015-07-29 16:21:19 +0530

    u -vhk

--- inst/www/js/doublelist.js
+++ inst/www/js/doublelist.js
@@ -1,4 +1,8 @@
-// globally declare the variables 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */// globally declare the variables 
 var data;
  var dim_data;
  $(document).ready(function () {
