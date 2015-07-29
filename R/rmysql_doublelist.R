4ab2eeecaee25c061049de4ad341efeba08a1695
Steven <steven.suting@gmail.com>
2015-07-29 18:41:03 +0530

    ss

--- R/rmysql_doublelist.R
+++ R/rmysql_doublelist.R
@@ -1,4 +1,4 @@
-rmysql<-function(){
+rmysql_doublelist<-function(){
 mydb = dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
 rs = dbSendQuery(mydb, "select * from product");
 data = fetch(rs, n=-1);
