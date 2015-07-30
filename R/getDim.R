d10ac5c0289ad5a0eef1399737c4af108ffca9ef
Steven <steven.suting@gmail.com>
2015-07-30 13:38:08 +0530

    ss

---
+++ R/getDim.R
@@ -0,0 +1,14 @@
+getDim<-function(dim){
+mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
+
+#Option to aggregate in mysql
+#rs = dbSendQuery(mydb, paste0( "select " ,dim, ", sum(" ,measures, ") from product group by ",dim," " , sep=""));
+
+rs = dbSendQuery(mydb, paste0( "select Distinct( " ,dim, ") from product" , sep=""));
+dataset <- fetch(rs, n=-1);
+
+names(dataset)[1] <-paste("Dimension");
+
+DimJSON<-toJSON(dataset)
+return(DimJSON);
+}
---
+++ R/getMeasure.R
@@ -0,0 +1,18 @@
+getMeasure<-function(dimension,measure){
+mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
+
+#Option to aggregate in mysql
+#rs = dbSendQuery(mydb, paste0( "select " ,dim, ", sum(" ,measures, ") from product group by ",dim," " , sep=""));
+
+rs = dbSendQuery(mydb, paste0( "select ( " ,measure, ") from product" , sep=""));
+dataset <- fetch(rs, n=-1);
+
+#aggregate in R
+# dataset[,2] is measure and dataset[,1] is Dimension
+aggdata <- aggregate(dataset[,2]~dimension,data=dataset,FUN=sum);
+names(aggdata)[1] <-paste("Dimension");
+names(aggdata)[2] <-paste("Measure");
+
+aggJSON<-toJSON(aggdata)
+return(aggJSON);
+}
