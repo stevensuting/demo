5c1af0ff572587d24834afc51b551a92084accb7
hkvin <hkvvin@gmail.com>
2015-07-30 17:13:38 +0530

    u -vhk

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -31,8 +31,8 @@ app.controller("AppController", function ($scope, $http) {
                                             char222[i] = obj[i]._row;
                                         }
                                     }
-                                    $scope.UsersData = char111;
-                                    $scope.UsersMeasure = char222;
+                                    $scope.UsersData = char111.join('');
+                                    $scope.UsersMeasure = char222.join('');
                                     alert("first " + $scope.UsersData);
                                     alert("second " + $scope.UsersMeasure);
                                 });
