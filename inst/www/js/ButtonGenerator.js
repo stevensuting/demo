1c134d4365b21cc8bb6224bce6b1a8ec4db270e0
santosh.k <santosh.k@winnow.in>
2015-07-30 15:39:09 +0530

    u-santosh

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -7,101 +7,69 @@ var app = angular.module('Demo', ['ui.bootstrap', 'ui.directives']);
 app.controller("AppController", function ($scope, $http) {
     $scope.loadUsersrefresh = function ()
     {
-var req = ocpu.call("rmysql_doublelist",{},
+        var req = ocpu.call("rmysql_doublelist", {},
                 // myname : myname
                         function (session) {
                             //to print in r console whatever returned by r 
                             session.getConsole(function (outtxt) {
                                 $("#output").text(outtxt);
-                         session.getObject(function(data){
-        //data is the object returned by the R function
-         // copy the object data to dim_data
-         
-          dim_data= data;
-          
-          obj=JSON.parse(dim_data);
-//          alert(obj.Variable_Type);
-//          alert(obj._row);
-//$scope.UsersData = obj;
-for (var i in obj) {
-   // if(obj.Variable_Type="cha
-  
-//                output = obj[i].Variable_Type;
-////                alert(obj.Variable_Type);
-//                alert(output);
-                if(obj[i].Variable_Type ==="character")
-                {
-                   
-                    alert("1  "+obj[i]._row);
-                  char111[i] = obj[i]._row; 
-//                  alert(char111.join(''));
-                            
-                  
-                }
-                else{
-                    alert("0  "+obj[i]._row);
-                }
-            }
-//            char11=trim(char111,',');
-            char111=trim(char111,',');
-//            alert("value== "+ char11);
-            alert("value== "+ char111);
-            $scope.UsersData = char111;
-alert("first "+$scope.UsersData);
-     });
-//     $scope.UsersData = JSON.parse(dim_data);
-     });
+                                session.getObject(function (data) {
+                                    //data is the object returned by the R function
+                                    // copy the object data to dim_data
 
-                            //if R returns an error, alert the error message
-//                            req.fail(function () {
-//                                alert("Server error: " + req.responseText);
-//                            });
-//$scope.UsersData = JSON.parse(dim_data);
+                                    dim_data = data;
+                                    obj = JSON.parse(dim_data);
+                                    for (var i in obj) {
+                                        if (obj[i].Variable_Type === "character")
+                                        {
+                                            alert("1  " + obj[i]._row);
+                                            char111[i] = obj[i]._row;
+                                        }
+                                        else {
+                                            alert("0  " + obj[i]._row);
+                                        }
+                                    }
+                                    $scope.UsersData = char111;
+                                    alert("first " + $scope.UsersData);
+                                });
+                            });
                         });
-//            var output;
-//            obj = JSON.parse(dim_data);
-//            alert("second"+dim_data);
 
-//$scope.UsersData = JSON.parse(dim_data);
-//alert($scope.UsersData);
-//            for (var i in obj) {
-//                output = obj.char;
-////                alert(obj.int);
-//            }
-    };
-    
+            };
+
     $scope.Clicked = function ()
     {
         buttonname = event.target.name;
         alert(buttonname);
         var dim = buttonname;
         var req = ocpu.call("getaggrdata", {
-                        dim: dim
-                    }, function (session) {
-                        session.getConsole(function(outtxt){
-                            $("#output1").text(outtxt);
-                            session.getObject(function(dimen)
-                            {
-                                dimen_data= dimen;
-          
-          obj1=JSON.parse(dimen_data);
+            dim: dim
+        }, function (session) {
+            session.getConsole(function (outtxt) {
+                $("#output1").text(outtxt);
+                session.getObject(function (dimen)
+                {
+                    dimen_data = dimen;
+
+                    obj1 = JSON.parse(dimen_data);
 //          alert(obj.Variable_Type);
 //          alert(obj._row);
+                    alert(obj1);
 
-for (var j in obj1) {
-                output = obj1[j]._row;
+                    for (var j in obj1) {
+                        output = obj1[j].dim;
 //                alert(obj._row);
-                alert(output);
-            }
+                        alert(output);
+                    }
 
-                            });
-                        });
-                       
-                    });
-        
-        
-        
+                });
+            });
+
+        });
+
+
+
     };
-    
+
 });
 
