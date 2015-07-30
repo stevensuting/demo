767fdaaa6464fe90aa2f364b0799b5a12d4c7a1f
hkvin <hkvvin@gmail.com>
2015-07-30 12:39:55 +0530

    u -vhk

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -19,15 +19,22 @@ var req = ocpu.call("rmysql_doublelist",{},
           dim_data= data;
           
           obj=JSON.parse(dim_data);
-          alert(obj.Variable_Type);
+//          alert(obj.Variable_Type);
 //          alert(obj._row);
-$scope.UsersData = obj;
+//$scope.UsersData = obj;
 for (var i in obj) {
    // if(obj.Variable_Type="cha
   
                 output = obj[i].Variable_Type;
 //                alert(obj.Variable_Type);
                 alert(output);
+                if(output==="character")
+                {
+                    alert("1");
+                }
+                else{
+                    alert("0");
+                }
             }
 alert("first "+$scope.UsersData);
      });
