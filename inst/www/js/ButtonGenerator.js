2a78f49facb6e7e2e801756e6fb4836cf37da2d1
hkvin <hkvvin@gmail.com>
2015-07-31 11:36:34 +0530

    u -vhk

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -92,7 +92,7 @@ app.controller("AppController", function ($scope, $http) {
 //        var dime = buttonname1;
         
         var req1 = ocpu.call("getMeasure", {
-           dimension: buttonname,
+         dimension: buttonname,
            measure: buttonname1
             
         }, function (session) {
