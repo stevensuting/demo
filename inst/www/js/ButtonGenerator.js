0d40a49bd2ea4225738f7ed0d3dc4134aaa37f7f
hkvin <hkvvin@gmail.com>
2015-07-31 15:46:12 +0530

    u -vhk

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -56,7 +56,7 @@ app.controller("AppController", function ($scope, $http) {
         alert(buttonname);
         var dim1 = buttonname;
         alert(dim1);
-        var req2= ocpu.call("getDim", {
+        var req2= ocpu.call("getDimension", {
             dim: dim1
         }, function (session) {
             session.getConsole(function (outtxt) {
