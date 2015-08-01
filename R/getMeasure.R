9e986a320593540a43f98fab8660aae3c31c240d
santosh.k <santosh.k@winnow.in>
2015-08-01 15:16:33 +0530

    u-santosh

--- R/getMeasure.R
+++ R/getMeasure.R
@@ -1,28 +1,25 @@
 getMeasure<-function(dimension,measure){
-#mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
+mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
 
-##Option to aggregate in mysql
-##rs = dbSendQuery(mydb, paste0( "select " ,dim, ", sum(" ,measures, ") from product group by ",dim," " , sep=""));
+#Option to aggregate in mysql
+#rs = dbSendQuery(mydb, paste0( "select " ,dim, ", sum(" ,measures, ") from product group by ",dim," " , sep=""));
 
-#rs = dbSendQuery(mydb, paste0( "select " ,dimension, "," ,measure, " from product " , sep=""));
+rs = dbSendQuery(mydb, paste0( "select " ,dimension, "," ,measure, " from product " , sep=""));
 
-#dataset <- fetch(rs, n=-1);
+dataset <- fetch(rs, n=-1);
 
-##aggregate in R
-## dataset[,2] is measure and dataset[,1] is Dimension
-#aggdata <- aggregate(dataset[,2]~dataset[,1],data=dataset,FUN=sum);
+#aggregate in R
+# dataset[,2] is measure and dataset[,1] is Dimension
+aggdata <- aggregate(dataset[,2]~dataset[,1],data=dataset,FUN=sum);
 
-#names(aggdata)[1] <-paste("Dimension");
-#names(aggdata)[2] <-paste("Measure");
+names(aggdata)[1] <-paste("Dimension");
+names(aggdata)[2] <-paste("Measure");
 
-#aggMeasure <- data.frame(aggdata$Measure);
-#names(aggMeasure)[1] <-paste("Measure");
+aggMeasure <- data.frame(aggdata$Measure);
+names(aggMeasure)[1] <-paste("Measure");
 
+#aggMeasureChar <- as.character(aggMeasure);
 
-dim <- dimension;
-mea<-measure;
-t <-c(dim,mea);
-#aggJSON<-toJSON(aggMeasure)
-#return(aggJSON);
-return(t);
+aggJSON<-toJSON(aggMeasure)
+return(aggJSON);
 }
