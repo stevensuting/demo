287b269f89f84924bbbef94a3eb6d5e6bd3c0669
santosh.k <santosh.k@winnow.in>
2015-08-03 13:05:34 +0530

    u-santosh

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -105,8 +105,8 @@ app.controller("AppController", function ($scope, $http) {
         var data = [{"data": 3.0}, {"data": 2.0}, {"data": 3.5}, {"data": 1.5}];
 
 // Highcharts requires the y option to be set
-        $.each(data, function (i, point) {
-            point.y = point.data;
+        $.each(obj1, function (i, point) {
+            point.y = point.Dimension;
         });
 
 
@@ -115,9 +115,9 @@ app.controller("AppController", function ($scope, $http) {
                 renderTo: 'container'
 //        type: 'pie'
             },
-            xAxis: {
-                categories: data
-            },
+//            xAxis: {
+//                categories: data
+//            },
             series: [{
                     data: data
                 }]
