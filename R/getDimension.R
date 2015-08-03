2b6c9d5c78a6472cd7fd1f74356bd86936bfa07a
Steven <steven.suting@gmail.com>
2015-08-03 19:44:22 +0530

    ss

--- R/getDimension.R
+++ R/getDimension.R
@@ -1,16 +1,17 @@
 getDimension<-function(dimension){
-mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
+#mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
+
+mydb_MSSQL = dbConnect(SQLServer(),"192.168.2.113/MicrosoftBIRetailDemo;user=appuser;password=appuser123");
 
 #Option to aggregate in mysql
 #rs = dbSendQuery(mydb, paste0( "select *from product"));
 
-rs = dbSendQuery(mydb, paste0( "select Distinct( " ,dimension, ") from product" , sep=""));
-dataset <- fetch(rs, n=-1);
+rs = dbSendQuery(mydb, paste0( "select Distinct( " ,dimension, ") from FactOnlineSales" , sep=""));
+dataset <- fetch(rs, n=10000);
 dimensionExt <- dataset[,1]
 names(dataset)[1] <-paste("Dimension");
 
 
 DimJSON<-toJSON(dataset)
 return(dimensionExt);
-
 }
--- R/getMeasure.R
+++ R/getMeasure.R
@@ -1,11 +1,12 @@
 getMeasure<-function(dimension,measure){
-mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
-
+#mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore', host='192.168.2.45');
+mydb_MSSQL = dbConnect(SQLServer(),"192.168.2.113/MicrosoftBIRetailDemo;user=appuser;password=appuser123");
 #Option to aggregate in mysql
 #rs = dbSendQuery(mydb, paste0( "select " ,dimension, ", sum(" ,measure, ") from product group by ",dimension," " , sep=""));
+#rs = dbSendQuery(mydb, paste0( "select " ,dimension, "," ,measure, " from product " , sep=""));
+rs = dbSendQuery(mydb_MSSQL, paste0( "select " ,dimension, "," ,measure, " from FactOnlineSales " , sep=""));
 
-rs = dbSendQuery(mydb, paste0( "select " ,dimension, "," ,measure, " from product " , sep=""));
-dataset <- fetch(rs, n=-1);
+dataset <- fetch(rs, n=10000);
 names(dataset)[1] <-paste("Dimension");
 names(dataset)[2] <-paste("Measure");
 
