fe5c7b42fe06b560e1a6f2f950640084dda3adac
Steven <steven.suting@gmail.com>
2015-08-03 11:18:27 +0530

    ss

--- R/getMeasure.R
+++ R/getMeasure.R
@@ -5,21 +5,20 @@ mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore
 #rs = dbSendQuery(mydb, paste0( "select " ,dim, ", sum(" ,measures, ") from product group by ",dim," " , sep=""));
 
 rs = dbSendQuery(mydb, paste0( "select " ,dimension, "," ,measure, " from product " , sep=""));
-
 dataset <- fetch(rs, n=-1);
 
 #aggregate in R
-# dataset[,2] is measure and dataset[,1] is Dimension
-aggdata <- aggregate(dataset[,2]~dataset[,1],data=dataset,FUN=sum);
+#dataset[,2] is measure and dataset[,1] is Dimension
+#aggdata <- aggregate(dataset[,2],dataset[,1],data=dataset,FUN=sum);
+aggdata <- aggregate(list(Measure=dataset[,2]),list(Dimension=dataset[,1]),sum);
 
-names(aggdata)[1] <-paste("Dimension");
-names(aggdata)[2] <-paste("Measure");
+#names(aggdata)[1] <-paste("Dimension");
+#names(aggdata)[2] <-paste("Measure");
 
 aggMeasure <- data.frame(aggdata$Measure);
+
 names(aggMeasure)[1] <-paste("Measure");
 
-#aggMeasureChar <- as.character(aggMeasure);
-
-aggJSON<-toJSON(aggMeasure)
+aggJSON<-toJSON(aggMeasure);
 return(aggJSON);
 }
