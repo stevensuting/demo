bf541093f4f2079c5d186b9aac3850c1e27d9b01
santosh.k <santosh.k@winnow.in>
2015-08-03 15:53:54 +0530

    u-santosh

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -116,11 +116,11 @@ app.controller("AppController", function ($scope, $http) {
         var series = [];
               
         for (var i = 0; i < obj1.length; i++) {
-            alert("obj value "+obj1[i].Dimension);
+//            alert("obj value "+obj1[i].Dimension);
             series.push({"data": obj1[i].Dimension});
         }
-        alert("series value "+series);
-        Highcharts.charts[0].xAxis[0].update({categories: series}, true);
+        alert("series value "+data);
+        Highcharts.charts[0].xAxis[0].update({categories: data}, true);
 
     };
 
