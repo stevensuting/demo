746fc490f017435611fdaba3dee31194dc3d8c13
Steven <steven.suting@gmail.com>
2015-08-04 21:19:46 +0530

    ss

--- R/getDimension.R
+++ R/getDimension.R
@@ -4,7 +4,7 @@ mydb <- dbConnect(MySQL(), user='appuser', password='appuser123', dbname='mstore
 #Option to aggregate in mysql
 #rs = dbSendQuery(mydb, paste0( "select *from product"));
 
-rs = dbSendQuery(mydb, paste0( "select Distinct( " ,dimension, ") from product" , sep=""));
+rs = dbSendQuery(mydb, paste0( "select Distinct( " ,dimension, ") from VW_RetailDataset" , sep=""));
 dataset <- fetch(rs, n=-1);
 dimensionExt <- dataset[,1]
 return(dimensionExt);
--- R/getMeasure.R
+++ R/getMeasure.R
@@ -4,23 +4,14 @@ mydb_MSSQL = dbConnect(SQLServer(),"192.168.2.113/MicrosoftBIRetailDemo;user=app
 #Option to aggregate in mysql
 #rs = dbSendQuery(mydb, paste0( "select " ,dimension, ", sum(" ,measure, ") from product group by ",dimension," " , sep=""));
 #rs = dbSendQuery(mydb, paste0( "select " ,dimension, "," ,measure, " from product " , sep=""));
-rs = dbSendQuery(mydb_MSSQL, paste0( "select " ,dimension, "," ,measure, " from FactOnlineSales " , sep=""));
 
+rs = dbSendQuery(mydb_MSSQL, paste0( "select " ,dimension, "," ,measure, " from VW_RetailDataset " , sep=""));
 dataset <- fetch(rs, n=10000);
 names(dataset)[1] <-paste("Dimension");
 names(dataset)[2] <-paste("Measure");
-
 aggData<- ddply(dataset,.(Dimension), summarize, sum = sum(Measure) )
-
 measureExt <- aggData[,2]
-#aggregate in R
-#dataset[,2] is measure and dataset[,1] is Dimension
-#aggdata <- aggregate(dataset[,2]~ dataset[,1],data=dataset,FUN=sum);
-#aggdata <- aggregate(list(Measure=dataset[,2]),list(Dimension=dataset[,1]),sum);
-
-#aggMeasure <- data.frame(dataset[,2]);
-#names(aggMeasure)[1] <-paste("Measure");
-
-#aggJSON<-toJSON(aggMeasure);
 return(measureExt);
+
+
 }
