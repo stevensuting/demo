a998868ed148d61dfd872b8a4d788a1ff806d967
santosh-winnow <santosh.k@winnow.in>
2015-08-07 14:19:16 +0530

    u-santosh

--- inst/www/js/ButtonGenerator.js
+++ inst/www/js/ButtonGenerator.js
@@ -148,6 +148,39 @@ chart.redraw();
         Highcharts.charts[0].series[0].tooltipFormatter = function(item) { return "hiiiiii "+buttonname1+"</b><br/>"; }
         chart.redraw();
     };
+    
+    
+    var mockDataForThisTest = "json=" + encodeURI(JSON.stringify([
+    {
+    id: 1,
+    firstName: "Peter",
+    lastName: "Jhons"},
+{
+    id: 2,
+    firstName: "David",
+    lastName: "Bowie"}
+]));
+
+
+function PeopleCtrl($scope, $http) {
+
+    $scope.people = [];
+
+    $scope.loadPeople = function() {
+        var httpRequest = $http({
+            method: 'POST',
+            url: '/echo/json/',
+            data: mockDataForThisTest
+
+        }).success(function(data, status) {
+            $scope.people = data;
+        });
+
+    };
+
+}
+    
+    
 });
 
 var buttonname333 = null;
